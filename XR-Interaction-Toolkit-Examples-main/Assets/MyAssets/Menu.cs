c8924f139d134f84aa837554bb0fcc414a3bfcf8
dev-stagiaire-idskillz <128144647+dev-stagiaire-idskillz@users.noreply.github.com>
2023-04-13 17:48:06 +0200

    Menu Play Button Fix

--- XR-Interaction-Toolkit-Examples-main/Assets/MyAssets/Menu.cs
+++ XR-Interaction-Toolkit-Examples-main/Assets/MyAssets/Menu.cs
@@ -23,7 +23,7 @@ public class Menu : MonoBehaviour
     public IEnumerator launch()
     {
         click.Play(0);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.2f);
         SceneManager.LoadScene("Game");
     }
 
